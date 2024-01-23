b509efaaeeae2ded48a91c45b2f09b23a0bfb469
Yoav Tzfati <yoav.tzfati@gmail.com>
2024-01-23 15:21:10 -0800

    script for formatting the dataset

---
+++ amps/mathematica/format_dataset.py
@@ -0,0 +1,34 @@
+from concurrent.futures import ThreadPoolExecutor
+from pathlib import Path
+
+from simplify_mathematica_tex import simplify_tex  # type: ignore
+
+
+def main():
+    formatted_dir = Path("amps/mathematica/formatted")
+    formatted_dir.mkdir(exist_ok=True)
+    for domain in Path("amps/mathematica").iterdir():
+        if not domain.is_dir():
+            continue
+        for problem_class in domain.iterdir():
+            if not problem_class.name.endswith("w_steps"):
+                continue
+            print(domain.name, problem_class.name)
+            out_dir = formatted_dir / domain.name / problem_class.name
+            out_dir.mkdir(exist_ok=True, parents=True)
+
+            def handle_file(file):
+                problem_text = file.read_text()
+                try:
+                    simplified = simplify_tex(problem_text)
+                except ValueError:
+                    return
+                out_file = out_dir / file.name
+                out_file.write_text(simplified)
+
+            with ThreadPoolExecutor(max_workers=8) as executor:
+                executor.map(handle_file, problem_class.glob("*.t??"))
+
+
+if __name__ == "__main__":
+    main()
--- amps/mathematica/gather_problem_stats.py
+++ amps/mathematica/gather_problem_stats.py
@@ -5,37 +5,43 @@ import tiktoken
 
 tokenizer = tiktoken.encoding_for_model("gpt-4")  # just to get an estimate
 
-csv_file = open("amps/mathematica/problem_stats.csv", "w")
-csv_writer = csv.DictWriter(
-    csv_file,
-    fieldnames=["domain", "problem_class", "problem", "num_steps", "num_tokens"],
-)
-csv_writer.writeheader()
 
-# Note - this only works before formatting
-identify_step = """\\end{array}
- \\\\
- """
+def main():
+    csv_file = open("amps/mathematica/problem_stats.csv", "w")
+    csv_writer = csv.DictWriter(
+        csv_file,
+        fieldnames=["domain", "problem_class", "problem", "num_steps", "num_tokens"],
+    )
+    csv_writer.writeheader()
 
-for domain in Path("amps/mathematica").iterdir():
-    if not domain.is_dir():
-        continue
-    for problem_class in domain.iterdir():
-        if not problem_class.name.endswith("w_steps"):
+    # Note - this only works before formatting
+    identify_step = """\\end{array}
+    \\\\
+    """
+
+    for domain in Path("amps/mathematica").iterdir():
+        if not domain.is_dir():
             continue
-        print(domain.name, problem_class.name)
-        for problem in problem_class.glob("*.txt"):
-            problem_text = problem.read_text()
-            num_steps = problem_text.count(identify_step) + 1
-            num_tokens = len(tokenizer.encode(problem_text))
-            csv_writer.writerow(
-                {
-                    "domain": domain.name,
-                    "problem_class": problem_class.name.removesuffix("_w_steps"),
-                    "problem": problem.name.removesuffix(".txt"),
-                    "num_steps": num_steps,
-                    "num_tokens": num_tokens,
-                }
-            )
+        for problem_class in domain.iterdir():
+            if not problem_class.name.endswith("w_steps"):
+                continue
+            print(domain.name, problem_class.name)
+            for problem in problem_class.glob("*.txt"):
+                problem_text = problem.read_text()
+                num_steps = problem_text.count(identify_step) + 1
+                num_tokens = len(tokenizer.encode(problem_text))
+                csv_writer.writerow(
+                    {
+                        "domain": domain.name,
+                        "problem_class": problem_class.name.removesuffix("_w_steps"),
+                        "problem": problem.name.removesuffix(".txt"),
+                        "num_steps": num_steps,
+                        "num_tokens": num_tokens,
+                    }
+                )
 
-csv_file.close()
+    csv_file.close()
+
+
+if __name__ == "__main__":
+    main()
